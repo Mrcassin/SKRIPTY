47469008074064f416f4b4fbf2d2941b7b8afa57
Mrcassin <msimkovsky@seznam.cz>
2017-03-15 22:27:46 +0100

    Add files via upload

---
+++ magic.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "♦ ♦ ♦ ♦ ♦ Welcome in script for editing photos. ♦ ♦ ♦ ♦ ♦"
+echo "♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦"
+echo "♦                                                       ♦"
+echo "♦                     FUNCTIONS                         ♦"
+echo "♦                                                       ♦"
+echo "♦         (1) Conversion to other format                ♦"
+echo "♦         (2) Conversion quality (from 1% - 100%)       ♦"
+echo "♦         (3) Resize image to any resolution            ♦"
+echo "♦         (4) Rotate an image                           ♦"
+echo "♦         (5) Effect charcoal                           ♦"
+echo "♦         (6) Effect implode                            ♦"
+echo "♦                                                       ♦"
+echo "♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦ ♦"
+echo "Yoour choice:"
+read choice
+
